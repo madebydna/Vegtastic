18ced474035a6d496d019cd1ecb2ae086c45b46e
dev singh <admin@kinfonet.org>
2010-09-06 11:49:50 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vegtastic::Application.config.secret_token = '4b6f7dcd8541f573846cc81dd2b15b8aaffb267fd35e57b61eaed4d2e8606d3a62c2a1a14dae7c34d4ca7776a4b3ab778847bd8c4c133d296dcff08f124167ee'
